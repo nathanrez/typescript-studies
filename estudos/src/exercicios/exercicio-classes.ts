447bfec924f9fd02390fd498f7045d23cd8d3f69
Nathan Rezende <nathanrezs@gmail.com>
2026-01-28 02:17:06 -0300

    Add files via upload

--- estudos/src/exercicios/exercicio-classes.ts
+++ estudos/src/exercicios/exercicio-classes.ts
@@ -20,7 +20,7 @@ class Vehicle implements IVehicle {
 }
 
 const newVehicle = new Vehicle ("Toyota", 1998);
-newVehicle.getInfo(); */
+newVehicle.getInfo(); 
 
 // ex.2
 interface IProduct {
@@ -44,4 +44,39 @@ class Product implements IProduct {
 }
 
 const product = new Product ("banana", 4);
-console.log(product.getPriceTag());
+console.log(product.getPriceTag()); */
+
+// 3.
+interface IAccount {
+    id: number;
+    getBalance(): number;
+}
+
+class Account implements IAccount {
+    public id: number;
+    private balance: number;
+
+    constructor(id: number, balance: number){
+        this.id = id;
+        this.balance = balance;
+    }
+
+    getBalance(): number {
+        return this.balance;
+    }
+
+    deposit(amount: number): void {
+        this.balance += amount;
+    }
+
+    withdraw(amount: number): void {
+        this.balance -= amount;
+    }
+}
+
+class SavingsAccount extends Account {
+    applyInterest(rate: number): void {
+        const interest = this.getBalance() * rate;
+        this.deposit(interest);
+    }
+}
